211ac02dccc4c54fce39441fd1278ac37d9ab876
Orbital_sFear <ldupin@rhino.(none)>
2008-07-02 14:31:32 -0700

    Giving the user a way to load up hash already generated

--- dash.rb
+++ dash.rb
@@ -1,4 +1,10 @@
 class Dash < Hash
+		#Load in a user's hash if they give me one
+	def initialize( hash = nil )
+		hash.each { |k, v| self[k] = v } if hash.is_a? Hash
+	end
+
+		#Attempt to index this guy via the the index requested
 	def method_missing( sym, *args )
 		(args.size == 0)? self[sym]: self[sym] = args[0]
 	end
