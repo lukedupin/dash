b0a517ab9b2591dc4138b7051c09951579130c2f
Orbital_sFear <ldupin@rhino.(none)>
2008-07-03 11:48:04 -0700

    Updated the program to deal souly with symbols, major speed increase

--- dash.rb
+++ dash.rb
@@ -1,11 +1,15 @@
 class Dash < Hash
 		#Load in a user's hash if they give me one
 	def initialize( hash = nil )
-		hash.each { |k, v| self[k] = v } if !hash.nil?
+		hash.each { |k, v| self[k.to_sym] = v } if !hash.nil?
 	end
 
 		#Attempt to index this guy via the the index requested
 	def method_missing( sym, *args )
-		(args.size == 0)? self[sym] || self[sym.to_s]: self[sym] = args[0]
+		if args.size == 0
+			return self[sym]
+		else
+			return self[sym.to_s[0..-2].to_sym] = args[0]
+		end
 	end
 end
