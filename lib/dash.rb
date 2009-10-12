4f38ad8bd4994365425f2c862eae2101c37dc8ac
Luke Dupin <ldupin@CRF450r.(none)>
2009-10-12 16:09:42 -0700

    Added in the ability to addres the underlying array directly

--- lib/dash.rb
+++ lib/dash.rb
@@ -6,8 +6,26 @@ class Dash < Hash
 		end if !hash.nil?
 	end
 
+    #Deal with array addresses
+  def []( key )
+    super
+  end
+
+    #Deal with array addresses
+  def []=( key, value )
+    super
+  end
+
 		#define id's
 	def id; self[:id] || self['id']; end; def id=(v); self[:id] = v; end;
+
+    #Transfer all symbol keys to string keys
+  def transfer
+    self.keys.each do |k|
+      self[k.to_s] = self[k] if k.is_a? Symbol
+    end
+    return self
+  end
 
 		#Define the respond to method
 	def respond_to?(v)
